b82b8561fc7fe4c3f4274af5f5b83a69dd8ff816
Asad <hmasad09@gmail.com>
2019-01-14 12:03:20 +0800

    Theme Applied to Home Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,300 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <meta charset="utf-8">
+  <title>ABC Company Pte.</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
 
-        <title>ABC Company Pte.</title>
+  <!-- Favicons -->
+  <link href="{{ asset('img-theme/favicon.png')}}" rel="icon">
+  <link href="{{ asset('img-theme/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+
+
+  <!-- Bootstrap CSS File -->
+  <link href="{{ asset('lib-theme/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="{{ asset('lib-theme/nivo-slider/css/nivo-slider.css') }}" rel="stylesheet">
+  <link href="{{ asset('lib-theme/owlcarousel/owl.carousel.css') }}" rel="stylesheet">
+  <link href="{{ asset('lib-theme/owlcarousel/owl.transitions.css') }}" rel="stylesheet">
+  <link href="{{ asset('lib-theme/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('lib-theme/animate/animate.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('lib-theme/venobox/venobox.css') }}" rel="stylesheet">
+
+  <!-- Nivo Slider Theme -->
+  <link href="{{ asset('css-theme/nivo-slider-theme.css') }}" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="{{ asset('css-theme/style.css') }}" rel="stylesheet">
+
+  <!-- Responsive Stylesheet File -->
+  <link href="{{ asset('css-theme/responsive.css') }}" rel="stylesheet">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+</head>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<body data-spy="scroll" data-target="#navbar-example">
 
-            .position-ref {
-                position: relative;
-            }
+  <div id="preloader"></div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+  <header>
+    <!-- header-area start -->
+    <div id="sticker" class="header-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12">
 
-            .content {
-                text-align: center;
-            }
+            <!-- Navigation -->
+            <nav class="navbar navbar-default">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".bs-example-navbar-collapse-1" aria-expanded="false">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+                <!-- Brand -->
+                <a class="navbar-brand page-scroll sticky-logo" href="/">
+                  <h4 style="color:WHITE;margin-top: 8px;">ABC Company Pte.</h4>
+                  <!-- Uncomment below if you prefer to use an image logo -->
+                  <!-- <img src="img/logo.png" alt="" title=""> -->
+								</a>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse main-menu bs-example-navbar-collapse-1" id="navbar-example">
+                <ul class="nav navbar-nav navbar-right">
 
-            .title {
-                font-size: 84px;
-            }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                
+                  @auth
+                    <li class="active">
+                       <a class="page-scroll" href="{{ url('/home') }}">Home</a>
+                    </li>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                    <li class="active">
+                       <a class="page-scroll" href="{{ route('login') }}">Login</a>
+                    </li>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+                    @if (Route::has('register'))
+                    <li class="active">
+                       <a class="page-scroll" href="{{ route('register') }}">Registrer</a>
+                    </li>
+                    @endif
+                  @endauth
+                
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                </ul>
+              </div>
+              <!-- navbar-collapse -->
+            </nav>
+            <!-- END: Navigation -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- header-area end -->
+  </header>
+  <!-- header end -->
+
+  <!-- Start Slider Area -->
+  <div id="home" class="slider-area">
+    <div class="bend niceties preview-2">
+      <div id="ensign-nivoslider" class="slides">
+        <img src="{{ asset('img-theme/slider/slider1.jpg') }}" alt="" title="#slider-direction-1" />
+        <img src="{{ asset('img-theme/slider/slider2.jpg') }}" alt="" title="#slider-direction-2" />
+        <img src="{{ asset('img-theme/slider/slider3.jpg') }}" alt="" title="#slider-direction-3" />
+      </div>
+
+      <!-- direction 1 -->
+      <div id="slider-direction-1" class="slider-direction slider-one">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content">
+                <!-- layer 1 -->
+                <div class="layer-1-1 hidden-xs wow slideInDown" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">The Best Business Information </h2>
+                </div>
+                <!-- layer 2 -->
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <h1 class="title2">We're In The Business Of Helping You Start Your Business</h1>
+                </div>
+                <!-- layer 3 -->
+                <div class="layer-1-3 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <a class="ready-btn right-btn page-scroll" href="#services">See Services</a>
+                  <a class="ready-btn page-scroll" href="#about">Learn More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- direction 2 -->
+      <div id="slider-direction-2" class="slider-direction slider-two">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content text-center">
+                <!-- layer 1 -->
+                <div class="layer-1-1 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">The Best Business Information </h2>
+                </div>
+                <!-- layer 2 -->
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <h1 class="title2">We're In The Business Of Get Quality Business Service</h1>
+                </div>
+                <!-- layer 3 -->
+                <div class="layer-1-3 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <a class="ready-btn right-btn page-scroll" href="#services">See Services</a>
+                  <a class="ready-btn page-scroll" href="#about">Learn More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- direction 3 -->
+      <div id="slider-direction-3" class="slider-direction slider-two">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content">
+                <!-- layer 1 -->
+                <div class="layer-1-1 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">The Best business Information </h2>
+                </div>
+                <!-- layer 2 -->
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <h1 class="title2">Helping Business Security  & Peace of Mind for Your Family</h1>
+                </div>
+                <!-- layer 3 -->
+                <div class="layer-1-3 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <a class="ready-btn right-btn page-scroll" href="#services">See Services</a>
+                  <a class="ready-btn page-scroll" href="#about">Learn More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Slider Area -->
+
+
+
+
+
+
+  <!-- Start Footer bottom Area -->
+  <footer>
+    <div class="footer-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="footer-content">
+              <div class="footer-head">
+                <div class="footer-logo">
+                  <h4>ABC Company Pte.</h4>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
+                <div class="footer-icons">
+                  <ul>
+                    <li>
+                      <a href="#"><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-google"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-pinterest"></i></a>
+                    </li>
+                  </ul>
                 </div>
+              </div>
             </div>
+          </div>
+          <!-- end single footer -->
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="footer-content">
+              <div class="footer-head">
+                <h4>information</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+                </p>
+                <div class="footer-contacts">
+                  <p><span>Tel:</span> +123 456 789</p>
+                  <p><span>Email:</span> contact@example.com</p>
+                  <p><span>Working Hours:</span> 9am-5pm</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end single footer -->
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="footer-content">
+              <div class="footer-head">
+                <h4>Instagram</h4>
+                <div class="flicker-img">
+                  <a href="#"><img src="{{ asset('img-theme/portfolio/1.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{ asset('img-theme/portfolio/2.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{ asset('img-theme/portfolio/3.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{ asset('img-theme/portfolio/4.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{ asset('img-theme/portfolio/5.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{ asset('img-theme/portfolio/6.jpg')}}" alt=""></a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </body>
+      </div>
+    </div>
+    <div class="footer-area-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="copyright text-center">
+              <p>
+                &copy; <strong>Abc Company Pte.</strong>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="{{ asset('lib-theme/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('lib-theme/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('lib-theme/owlcarousel/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('lib-theme/venobox/venobox.min.js') }}"></script>
+  <script src="{{ asset('lib-theme/knob/jquery.knob.js') }}"></script>
+  <script src="{{ asset('lib-theme/wow/wow.min.js') }}"></script>
+  <script src="{{ asset('lib-theme/parallax/parallax.js') }}"></script>
+  <script src="{{ asset('lib-theme/easing/easing.min.js') }}"></script>
+  <script src="{{ asset('lib-theme/nivo-slider/js/jquery.nivo.slider.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('lib-theme/appear/jquery.appear.js') }}"></script>
+  <script src="{{ asset('lib-theme/isotope/isotope.pkgd.min.js') }}"></script>
+
+  <script src="{{ asset('js-theme/main.js') }}"></script>
+</body>
+
 </html>
